89f5b948cd3645a8c7f899d12481618b8d8f9026
Arthur Vianna <arthurpedrosa96.ap@gmail.com>
2022-05-05 18:12:57 -0300

    ao clicar no botão enviar, o conteúdo do formulário é substituído pelas informações preenchidas

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const loginEmailInput = document.getElementById('email');
 const loginPasswordInput = document.getElementById('password');
 const loginSubmitButton = document.getElementById('login-submit-btn');
-const agreementCheckboxInput = document.getElementById('agreement');
+
+const form = document.getElementById('evaluation-form');
+const formNameInput = document.getElementById('input-name');
+const formLastNameInput = document.getElementById('input-lastname');
+const formEmailInput = document.getElementById('input-email');
+const formHouseSelectInput = document.getElementById('house');
+const formFamilyRadioInputs = document.getElementsByClassName('family-radio');
+const formSubjectsCheckboxInputs = document.getElementsByClassName('subject');
+const formRateRadioInputs = document.getElementsByClassName('rate-radio');
+const formCommentInput = document.getElementById('textarea');
+const formAgreementCheckboxInput = document.getElementById('agreement');
 const formSubmitButton = document.getElementById('submit-btn');
 
 function validateData(inputValue, expectedValue) {
@@ -20,12 +30,71 @@ function submitCredentialsToLogin() {
   }
 }
 
+function getSelectedRadioItemValue(radioList) {
+  for (let index = 0; index < radioList.length; index += 1) {
+    if (radioList[index].checked) {
+      return radioList[index].value;
+    }
+  }
+}
+
+function getListOfSubjects(checkboxList) {
+  const selectedSubjects = [];
+  for (let index = 0; index < checkboxList.length; index += 1) {
+    if (checkboxList[index].checked) {
+      selectedSubjects.push(checkboxList[index].value);
+    }
+  }
+  return selectedSubjects;
+}
+
+function getFormData() {
+  const selectedFamily = getSelectedRadioItemValue(formFamilyRadioInputs);
+  const selectedRate = getSelectedRadioItemValue(formRateRadioInputs);
+  const selectedSubjects = getListOfSubjects(formSubjectsCheckboxInputs);
+  const formData = {
+    name: `${formNameInput.value} ${formLastNameInput.value}`,
+    email: formEmailInput.value,
+    house: formHouseSelectInput.value,
+    family: selectedFamily,
+    subjects: selectedSubjects,
+    rate: selectedRate,
+    comment: formCommentInput.value,
+  };
+  return formData;
+}
+
+function addInfoToPage(prefix, info) {
+  const newPTag = document.createElement('p');
+  if (typeof info === 'object') {
+    newPTag.innerText = `${prefix}: ${info.join(', ')}`;
+  } else {
+    newPTag.innerText = `${prefix}: ${info}`;
+  }
+  newPTag.className = 'submited-info';
+  form.appendChild(newPTag);
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  const formData = getFormData();
+  form.innerHTML = '';
+  addInfoToPage('Nome', formData.name);
+  addInfoToPage('Email', formData.email);
+  addInfoToPage('Casa', formData.house);
+  addInfoToPage('Família', formData.family);
+  addInfoToPage('Matérias', formData.subjects);
+  addInfoToPage('Avaliação', formData.rate);
+  addInfoToPage('Observações', formData.comment);
+}
+
 loginSubmitButton.addEventListener('click', submitCredentialsToLogin);
-agreementCheckboxInput.addEventListener('change', (event) => {
-  console.log(event.target.checked);
+formAgreementCheckboxInput.addEventListener('change', (event) => {
   if (event.target.checked) {
     formSubmitButton.removeAttribute('disabled');
   } else {
     formSubmitButton.setAttribute('disabled', 'disabled');
   }
 });
+
+formSubmitButton.addEventListener('click', submitForm);
